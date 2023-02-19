805e71e336c42a65a1ffb198cd0fc24062570f74
Shaghayegh Ghasemi <shaghayegh5ghasemi@gmail.com>
2023-02-19 01:14:18 -0500

    class room completed

--- system.py
+++ system.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 class Account:
     def __init__(self, username, password) -> None:
         self.username = username
@@ -45,6 +48,10 @@ class BusinessOwner(Account):
         self.rooms = []
     
     def defineRoom(self, BusinessType, size, regularPrice):
+        newRoom = Room(BusinessType, size, regularPrice)
+        self.rooms.append(newRoom)
+    
+    def updateRevenue(self):
         pass
 
 class Room:
@@ -52,10 +59,41 @@ class Room:
         self.roomType = roomType 
         self.size = size
         self.regularPrice = regularPrice
-        self. map = []
-    
-    def createRoomMap(self):
-        pass 
+        self.map = np.zeros((self.size, self.size), dtype=int)
+        self.price = self.applyPrice()
+
+    def applyPrice(self):
+        price = np.full((self.size, self.size), self.regularPrice)
+        middle = math.floor(self.size/2)
+
+        # two middle columns
+        price[:, middle] = price[:, 0]*1.25
+        price[:, middle+1] = price[:, 1]*1.25 
+        # first two rows
+        price[0, :] = price[:, 0]*2 
+        price[1, :] = price[:, 1]*2 
+        # last two rows
+        price[-2, :] = price[:, 0]*0.75 
+        price[-1, :] = price[:, 1]*0.75
+
+        return price
+        
+    def updateVacancy(self, rows, columns, fill):
+        for i in range(len(rows)):
+            self.map[rows[i], columns[i]] = fill # if fill = 1 reserve seat otherwise make it available
+
+    def showMap(self):
+        arr = self.map
+        columnID = np.array([*range(0, self.size, 1)])
+        arr = np.append(arr, [columnID], axis=0)
+        columnID = np.append(columnID, 0)
+        rowID = columnID.reshape((len(columnID), 1))
+        arr = np.append(arr, rowID, axis=1)
+        
+        for a in arr:
+            for elem in a:
+                print("{}".format(elem).rjust(3), end="")
+            print(end="\n")
 
 class Ticket:
     def __init__(self, ticketID, username, roomType, seatDetail) -> None:
